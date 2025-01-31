fd21770771e8cadd6b37e4f8cd47aba50e5f46fd
Animesh Basak <basakanimesh16@gmail.com>
2025-02-01 01:01:26 +0530

    implement prediction route and add HTML template for displaying results

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -86,7 +86,7 @@ class DataTransformation:
             save_numpy_array_data(self.data_transformation_config.transformed_test_file_path, array = test_arr)
             save_object(self.data_transformation_config.transformed_object_file_path, obj = preprocessor_object)
             
-            
+            save_object("final_model/model.pkl", preprocessor_object)
             # preparing artifacts
             data_transformation_artifact = DataTransformationArtifact(
                 transformed_train_file_path = self.data_transformation_config.transformed_train_file_path,
